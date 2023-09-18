e4ae3ac3b478f3b53f7c6656574a34a16a101e0f
blakewoo <blakewoo0819@gmail.com>
2023-09-18 21:43:33 +0900

    Update codebasecode

--- src/codebase/modal.js
+++ src/codebase/modal.js
@@ -7,11 +7,13 @@ const JH_okcanelModal = (function () {
      * @param CANCEL:cancel버튼문구
      * @constructor
      */
-    function JH_okcanelModal (TITLE,EXPLAIN,OK,CANCEL) {
+    function JH_okcanelModal (TITLE,EXPLAIN,OK,CANCEL,WIDTH=300,HEIGHT=200) {
         this.TITLE = TITLE
         this.EXPLAIN = EXPLAIN
         this.OK_VALUE = OK
         this.CANCEL_VALUE = CANCEL
+        this.WIDTH = WIDTH
+        this.HEIGHT = HEIGHT
 
         this.paint()
     }
@@ -21,18 +23,38 @@ const JH_okcanelModal = (function () {
 
         let containerDiv = document.createElement("div")
         containerDiv.style.border="1px solid black"
-        containerDiv.style.borderRadius = "2px"
-        containerDiv.style.width = "300px"
-        containerDiv.style.height = "200px"
+        containerDiv.style.borderRadius = "5px"
+
+        if(this.WIDTH) {
+            containerDiv.style.width = this.WIDTH+"px"
+        }
+        else {
+            containerDiv.style.width = "300px"
+        }
+
+        if(this.HEIGHT) {
+            containerDiv.style.height = this.HEIGHT+"px"
+        }
+        else {
+            containerDiv.style.height = "200px"
+        }
+
         containerDiv.style.position = "absolute"
-        containerDiv.style.left = ""
-        containerDiv.style.top = ""
+        containerDiv.style.left = "calc(50% - "+Number(containerDiv.style.width.split("px")[0])/2+"px)"
+        containerDiv.style.top = "calc(50% - "+Number(containerDiv.style.height.split("px")[0])/2+"px)"
 
         let explainDiv = document.createElement("div")
-        explainDiv.innerText = this.EXPLAIN
         explainDiv.style.width = "100%"
         explainDiv.style.height = "85%"
         explainDiv.style.textAlign = "center"
+
+        let explainP = document.createElement("p")
+        explainP.style.position = "absolute"
+        explainP.style.left = "50%"
+        explainP.style.top = "40%"
+        explainP.style.transform = "translate(-50%,-50%)"
+        explainP.innerText = this.EXPLAIN
+        explainDiv.appendChild(explainP)
 
         let buttonContainerDiv = document.createElement("div")
         buttonContainerDiv.style.width = "100%"
@@ -41,13 +63,16 @@ const JH_okcanelModal = (function () {
 
         let okButton = document.createElement("input")
         okButton.type = "button"
-        okButton.addEventListener("click",this.okEvent.bind(this))
+        okButton.addEventListener("click",this.okEvent)
         okButton.value = this.OK_VALUE;
+        okButton.classList.add("jh_modal_button")
+        okButton.style.marginRight = "10px"
 
         let cancelButton = document.createElement("input")
         cancelButton.type = "button"
-        cancelButton.addEventListener("click",this.cancelEvent.bind(this))
+        cancelButton.addEventListener("click",this.cancelEvent)
         cancelButton.value = this.CANCEL_VALUE;
+        cancelButton.classList.add("jh_modal_button")
 
         buttonContainerDiv.appendChild(okButton)
         buttonContainerDiv.appendChild(cancelButton)
@@ -58,6 +83,8 @@ const JH_okcanelModal = (function () {
             explainDiv.style.height = "75%"
             titleDiv.style.height = "10%"
             titleDiv.style.textAlign = "center"
+            titleDiv.style.fontSize = "14pt"
+            titleDiv.style.fontWeight = "bold"
             containerDiv.appendChild(titleDiv)
 
         }
@@ -72,7 +99,7 @@ const JH_okcanelModal = (function () {
     }
 
     JH_okcanelModal.prototype.cancelEvent = function (event) {
-        this.parentNode.remove()
+        event.currentTarget.parentNode.parentNode.remove()
     }
 
     return JH_okcanelModal
